ec639be28f3f42cea7d228a21938e40c2ea9cc8d
fenggp <18319407429@163.com>
2023-06-20 18:22:09 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp, nextTick } from 'vue'
+import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from '@/App.vue'
 import './assets/main.css'
@@ -22,28 +22,3 @@ const router = createRouter({
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-let baseOffset = 0
-let extendOffset = 0
-let node = null
-let flag = false
-let timer = null
-// document.addEventListener('selectionchange', () => {
-//   if (timer) {
-//     return
-//   }
-//   const selection = window.getSelection()
-//   node = node || selection.baseNode
-//   baseOffset = selection.anchorOffset
-//   extendOffset = selection.focusOffset
-//   timer = setTimeout(() => {
-//     selection.empty()
-//     setTimeout(() => {
-//       const selection = window.getSelection()
-//       const range = new Range()
-//       range.setStart(node, baseOffset)
-//       range.setEnd(node, extendOffset)
-//       selection.addRange(range)
-//     })
-//   }, 2000)
-// })
